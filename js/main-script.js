80ba1622a8383ee39d3061ce15ed8bc6f7d6b092
JoseCruz2002 <jpcruz2002@gmail.com>
2023-06-03 21:25:01 +0100

    dd ovni, the ovni rotates and translates along the XX and ZZ axis

--- js/main-script.js
+++ js/main-script.js
@@ -5,11 +5,16 @@ var scene, renderer, camera;
 
 var mesh, geometry;
 
+var clock;
+
+// objects
+var ovni;
+
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene(){
-    'use strict';
+function createScene() {
+	'use strict';
 
 }
 
@@ -22,9 +27,9 @@ function createScene(){
 /* CREATE LIGHT(S) */
 /////////////////////
 function createAmbientLight() {
-    'use strict';
+	'use strict';
 
-    const light = new THREE.AmbientLight({color: 0xffe000, intensity: 3}); // soft white light
+	const light = new THREE.AmbientLight({ color: 0xffe000, intensity: 3 }); // soft white light
 	scene.add(light);
 }
 
@@ -32,58 +37,95 @@ function createAmbientLight() {
 /* CREATE OBJECT3D(S) */
 ////////////////////////
 function createOVNIbody(obj, x, y, z) {
-    'use strict';
+	'use strict';
 
-    var ud = obj.userData;
+	var ud = obj.userData;
 
-    geometry = new THREE.SphereGeometry();
-    geometry.scale(ud.r_body*2, ud.h_body, ud.r_body*2);
-    mesh = new THREE.Mesh(geometry, ud.materials[0]);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
+	geometry = new THREE.SphereGeometry();
+	geometry.scale(ud.r_body, ud.h_body, ud.r_body);
+	mesh = new THREE.Mesh(geometry, ud.materials_body.lambert_material_body);
+	mesh.position.set(x, y, z);
+	obj.add(mesh);
 }
 
 function createOVNIcockpit(obj, x, y, z) {
-    'use strict';
+	'use strict';
 
-    var ud = obj.userData;
+	var ud = obj.userData;
 
-    geometry = new THREE.SphereGeometry();
-    geometry.scale(ud.r_body*2, ud.h_body, ud.r_body*2);
-    mesh = new THREE.Mesh(geometry, ud.materials[0]);
-    mesh.position.set(x, y, z);
-    obj.add(mesh);
+	geometry = new THREE.SphereGeometry(ud.r_cockpit);
+	mesh = new THREE.Mesh(geometry, ud.materials_cc.lambert_material_cc);
+	mesh.position.set(x, y, z);
+	obj.add(mesh);
 }
 
+function createOVNIcylinder(obj, x, y, z) {
+	'use strict';
+
+	var ud = obj.userData;
+
+	geometry = new THREE.CylinderGeometry(ud.r_cylinder, ud.r_cylinder, ud.h_cylinder);
+	mesh = new THREE.Mesh(geometry, ud.materials_cc.lambert_material_cc);
+	mesh.position.set(x, y, z);
+	obj.add(mesh);
+}
+
+function createOVNIlight(obj, x, y, z) {
+	'use strict';
+
+	var ud = obj.userData;
+
+	geometry = new THREE.SphereGeometry(ud.r_light);
+	mesh = new THREE.Mesh(geometry, ud.materials_light.lambert_material_light);
+	mesh.position.set(x, y, z);
+	obj.add(mesh);
+}
 
 function createOVNI(x, y, z) {
-    'use strict';
+	'use strict';
 
-    var ovni = new THREE.Object3D();
+	ovni = new THREE.Object3D();
 
-    var lambert_material = new THREE.MeshLambertMaterial({color: 0xff0000, emissive: 20});
-    var phong_material = new THREE.MeshPhongMaterial({color: 0xffaa22, emissive: 20, specular: 15, shininess: 5});
-    var toon_material = new THREE.MeshToonMaterial({color: 0xffaa22});
+	var lambert_material_body = new THREE.MeshLambertMaterial({ color: 0x262725, emissive: 0x242923 });
+	var lambert_material_cc = new THREE.MeshLambertMaterial({ color: 0x323857, emissive: 0x242923 }); /*cylinder and cockpit*/
+	var lambert_material_light = new THREE.MeshLambertMaterial({ color: 0x610000, emissive: 0x242923 });
+	var phong_material_body = new THREE.MeshPhongMaterial({ color: 0xffaa22, emissive: 0x242923, specular: 15, shininess: 5 });
+	var phong_material_cc = new THREE.MeshPhongMaterial({ color: 0x323857, emissive: 0x242923, specular: 15, shininess: 5 });
+	var phong_material_light = new THREE.MeshPhongMaterial({ color: 0x610000, emissive: 0x242923, specular: 15, shininess: 5 });
+	var toon_material_body = new THREE.MeshToonMaterial({ color: 0xffaa22 });
+	var toon_material_cc = new THREE.MeshToonMaterial({ color: 0x323857 });
+	var toon_material_light = new THREE.MeshToonMaterial({ color: 0x610000 });
 
-    ovni.userData = {materials: {lambert_material, phong_material, toon_material}, r_body: 20, h_body: 10};
+	ovni.userData = {
+		materials_body: { lambert_material_body, phong_material_body, toon_material_body },
+		materials_cc: { lambert_material_cc, phong_material_cc, toon_material_cc },
+		materials_light: { lambert_material_light, phong_material_light, toon_material_light },
+		r_body: 5, h_body: 3,
+		r_cockpit: 2,
+		r_cylinder: 2, h_cylinder: 1.5,
+		r_light: 1,
+		y_rotate: Math.PI / 2,
+		move_plus_x: false, move_minus_x: false, move_plus_z: false, move_minus_z: false,
+		velocity: 10
+	};
 
-    createOVNIbody(ovni, 0, 0, 0);
-    //createOVNIcockpit(ovni, );
-    //createOVNIcylinder(ovni, );
-    //createOVNIlight(ovni, );
-    //createOVNIlight(ovni, );
-    //createOVNIlight(ovni, );
-    //createOVNIlight(ovni, );
+	createOVNIbody(ovni, 0, 0, 0);
+	createOVNIcockpit(ovni, 0, ovni.userData.h_body, 0);
+	createOVNIcylinder(ovni, 0, -ovni.userData.h_body, 0);
+	createOVNIlight(ovni, 0, -ovni.userData.h_body / 1.6, ovni.userData.r_body / 1.5);
+	createOVNIlight(ovni, ovni.userData.r_body / 1.5, -ovni.userData.h_body / 1.6, 0);
+	createOVNIlight(ovni, 0, -ovni.userData.h_body / 1.6, -ovni.userData.r_body / 1.5);
+	createOVNIlight(ovni, -ovni.userData.r_body / 1.5, -ovni.userData.h_body / 1.6, 0);
 
-    ovni.position.set(x, y, z);
-    scene.add(ovni);
+	ovni.position.set(x, y, z);
+	scene.add(ovni);
 }
 
 ////////////
 /* UPDATE */
 ////////////
-function update(){
-    'use strict';
+function update() {
+	'use strict';
 
 }
 
@@ -91,63 +133,113 @@ function update(){
 /* DISPLAY */
 /////////////
 function render() {
-    'use strict';
+	'use strict';
 
-    renderer.render(scene, camera);
+	renderer.render(scene, camera);
 }
 
 function createCamera() {
-    'use strict';
+	'use strict';
 
-    //perspective camera
-    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.x = 100;
-	camera.position.y = 100;
-	camera.position.z = 100;
+	//perspective camera
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.x = 10;
+	camera.position.y = -40;
+	camera.position.z = 10;
 	camera.lookAt(scene.position);
 }
 
 function createScene() {
-    'use strict';
+	'use strict';
 
-    scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
-    scene.add(new THREE.AxesHelper(100));
-    //scene.background = new THREE.Color(0xfffefe);
+	scene.add(new THREE.AxesHelper(50));
+	//scene.background = new THREE.Color(0xfffefe);
 
-    createAmbientLight();
-    createOVNI(0, 0, 0);
+	createAmbientLight();
+	createOVNI(0, 0, 0);
 }
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
 function init() {
-    'use strict';
+	'use strict';
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
 
-    createScene();
-    createCamera();
+	createScene();
+	createCamera();
+
+	clock = new THREE.Clock();
+
+	window.addEventListener("keydown", onKeyDown);
+	window.addEventListener("keyup", onKeyUp);
+	window.addEventListener("resize", onResize);
 }
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
 function animate() {
-    'use strict';
-    render();
-    //update
-    requestAnimationFrame(animate);
+	'use strict';
+
+	var delta = clock.getDelta();
+
+	rotateOVNI(delta);
+	moveOVNI(delta);
+
+	render();
+
+	requestAnimationFrame(animate);
+}
+
+function rotateOVNI(delta) {
+	'use strict';
+
+	ovni.rotation.y += ovni.userData.y_rotate * delta;
+
+}
+
+function moveOVNI(delta) {
+	'use strict';
+
+	var ud = ovni.userData;
+	var z_motion = 0, x_motion = 0;
+
+	if (ud.move_minus_z) {
+		//ovni.position.z -= ud.velocity * delta;
+		z_motion -= 1;
+	}
+	if (ud.move_plus_z) {
+		//ovni.position.z += ud.velocity * delta;
+		z_motion += 1;
+	}
+	if (ud.move_minus_x) {
+		//ovni.position.x -= ud.velocity * delta;
+		x_motion -= 1;
+	}
+	if (ud.move_plus_x) {
+		//ovni.position.x += ud.velocity * delta;
+		x_motion += 1;
+	}
+	if (z_motion != 0 || x_motion != 0) {
+		const v = new THREE.Vector3(x_motion, 0, z_motion).normalize();
+		ovni.position.x += v.x * ud.velocity * delta;
+		ovni.position.y += v.y * ud.velocity * delta;
+		ovni.position.z += v.z * ud.velocity * delta;
+	}
+
 }
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
-function onResize() { 
-    'use strict';
+function onResize() {
+	'use strict';
 
 }
 
@@ -155,14 +247,44 @@ function onResize() {
 /* KEY DOWN CALLBACK */
 ///////////////////////
 function onKeyDown(e) {
-    'use strict';
+	'use strict';
+
+	switch (e.keyCode) {
+		case 38: // arrow-up
+			ovni.userData.move_plus_z = true;
+			break;
+		case 40: // arrow-down
+			ovni.userData.move_minus_z = true;
+			break;
+		case 37: // arrow-left
+			ovni.userData.move_minus_x = true;
+			break;
+		case 39: // arrow-right
+			ovni.userData.move_plus_x = true;
+			break;
+	}
 
 }
 
 ///////////////////////
 /* KEY UP CALLBACK */
 ///////////////////////
-function onKeyUp(e){
-    'use strict';
+function onKeyUp(e) {
+	'use strict';
+
+	switch (e.keyCode) {
+		case 38: // arrow-up
+			ovni.userData.move_plus_z = false;
+			break;
+		case 40: // arrow-down
+			ovni.userData.move_minus_z = false;
+			break;
+		case 37: // arrow-left
+			ovni.userData.move_minus_x = false;
+			break;
+		case 39: // arrow-right
+			ovni.userData.move_plus_x = false;
+			break;
+	}
 
 }
