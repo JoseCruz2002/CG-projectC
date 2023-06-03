22c09e4af1c4d339e2b075a5f67fd51875a3322a
JoseCruz2002 <jpcruz2002@gmail.com>
2023-06-03 15:18:22 +0100

    add some initial functions and the base of the ovni object

--- js/main-script.js
+++ js/main-script.js
@@ -1,7 +1,9 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+var scene, renderer, camera;
 
+var mesh, geometry;
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -19,25 +21,62 @@ function createScene(){
 /////////////////////
 /* CREATE LIGHT(S) */
 /////////////////////
+function createAmbientLight() {
+    'use strict';
+
+    const light = new THREE.AmbientLight({color: 0xffe000, intensity: 3}); // soft white light
+	scene.add(light);
+}
 
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
-
-//////////////////////
-/* CHECK COLLISIONS */
-//////////////////////
-function checkCollisions(){
+function createOVNIbody(obj, x, y, z) {
     'use strict';
 
+    var ud = obj.userData;
+
+    geometry = new THREE.SphereGeometry();
+    geometry.scale(ud.r_body*2, ud.h_body, ud.r_body*2);
+    mesh = new THREE.Mesh(geometry, ud.materials[0]);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
 }
 
-///////////////////////
-/* HANDLE COLLISIONS */
-///////////////////////
-function handleCollisions(){
+function createOVNIcockpit(obj, x, y, z) {
     'use strict';
 
+    var ud = obj.userData;
+
+    geometry = new THREE.SphereGeometry();
+    geometry.scale(ud.r_body*2, ud.h_body, ud.r_body*2);
+    mesh = new THREE.Mesh(geometry, ud.materials[0]);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+}
+
+
+function createOVNI(x, y, z) {
+    'use strict';
+
+    var ovni = new THREE.Object3D();
+
+    var lambert_material = new THREE.MeshLambertMaterial({color: 0xff0000, emissive: 20});
+    var phong_material = new THREE.MeshPhongMaterial({color: 0xffaa22, emissive: 20, specular: 15, shininess: 5});
+    var toon_material = new THREE.MeshToonMaterial({color: 0xffaa22});
+
+    ovni.userData = {materials: {lambert_material, phong_material, toon_material}, r_body: 20, h_body: 10};
+
+    createOVNIbody(ovni, 0, 0, 0);
+    //createOVNIcockpit(ovni, );
+    //createOVNIcylinder(ovni, );
+    //createOVNIlight(ovni, );
+    //createOVNIlight(ovni, );
+    //createOVNIlight(ovni, );
+    //createOVNIlight(ovni, );
+
+    ovni.position.set(x, y, z);
+    scene.add(ovni);
 }
 
 ////////////
@@ -54,6 +93,30 @@ function update(){
 function render() {
     'use strict';
 
+    renderer.render(scene, camera);
+}
+
+function createCamera() {
+    'use strict';
+
+    //perspective camera
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.x = 100;
+	camera.position.y = 100;
+	camera.position.z = 100;
+	camera.lookAt(scene.position);
+}
+
+function createScene() {
+    'use strict';
+
+    scene = new THREE.Scene();
+
+    scene.add(new THREE.AxesHelper(100));
+    //scene.background = new THREE.Color(0xfffefe);
+
+    createAmbientLight();
+    createOVNI(0, 0, 0);
 }
 
 ////////////////////////////////
@@ -62,6 +125,12 @@ function render() {
 function init() {
     'use strict';
 
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createCamera();
 }
 
 /////////////////////
@@ -69,7 +138,9 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
-
+    render();
+    //update
+    requestAnimationFrame(animate);
 }
 
 ////////////////////////////
---
+++ js/texture-generation.js
@@ -0,0 +1,129 @@
+//////////////////////
+/* GLOBAL VARIABLES */
+//////////////////////
+
+var scene, renderer, camera;
+
+
+
+/////////////////////
+/* CREATE SCENE(S) */
+/////////////////////
+function createScene() {
+	'use strict';
+
+}
+
+//////////////////////
+/* CREATE CAMERA(S) */
+//////////////////////
+
+
+/////////////////////
+/* CREATE LIGHT(S) */
+/////////////////////
+
+function createLight() {
+	'use strict';
+
+	const light = new THREE.AmbientLight(0x404040); // soft white light
+	scene.add(light);
+}
+
+////////////////////////
+/* CREATE OBJECT3D(S) */
+////////////////////////
+
+function createTexture() {
+	'use strict';
+
+	const texture = 2;
+}
+
+
+////////////
+/* UPDATE */
+////////////
+
+/////////////
+/* DISPLAY */
+/////////////
+function render() {
+	'use strict';
+
+	renderer.render(scene, camera);
+
+}
+
+function createScene() {
+	'use strict';
+
+	scene = new THREE.Scene();
+
+	scene.add(new THREE.AxesHelper(100));
+	scene.background = new THREE.Color(0xffeeff);
+
+	createLight();
+	createGroundTexture();
+}
+
+function createCamera() {
+	'use strict';
+
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.x = window.innerWidth;
+	camera.position.y = window.innerHeight;
+	camera.position.z = window.innerWidth;
+	camera.lookAt(scene.position);
+}
+
+////////////////////////////////
+/* INITIALIZE ANIMATION CYCLE */
+////////////////////////////////
+function init() {
+	'use strict';
+
+	renderer = new THREE.WebGLRenderer({ antialias: true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	createScene();
+	createCamera();
+}
+
+/////////////////////
+/* ANIMATION CYCLE */
+/////////////////////
+function animate() {
+	'use strict';
+
+	// update
+
+	render();
+
+	requestAnimationFrame(animate);
+}
+
+////////////////////////////
+/* RESIZE WINDOW CALLBACK */
+////////////////////////////
+function onResize() {
+	'use strict';
+
+}
+
+///////////////////////
+/* KEY DOWN CALLBACK */
+///////////////////////
+function onKeyDown(e) {
+	'use strict';
+
+}
+
+///////////////////////
+/* KEY UP CALLBACK */
+///////////////////////
+function onKeyUp(e) {
+	'use strict';
+
+}
